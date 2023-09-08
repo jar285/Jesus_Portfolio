7634254cb741431c70983cc9d4e25f2fa7e2cf37
jar285 <jar285@njit.edu>
2023-09-07 22:18:14 -0400

    Updated Skills page

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import {inViewHandler} from "../animationHandler";
+import {motion,useAnimation} from "framer-motion";
+import {InView} from "react-intersection-observer";
+import { skills } from "../data";
 
 export const Skills = () => {
+  const animation = useAnimation();
+
   return (
-    <div>Skills</div>
-  )
-}
+   <InView onChange={(inView)=> inViewHandler(inView,animation)}>
+    <section className="h-76 bg-primary py-12">
+      <div className="container mx-auto">
+        <motion.div
+          className="grid grid-cols-6 md:grid-flow-row" 
+          inital={{opacity: 0, scale:0.5}}
+          animate={animation}
+        >
+          {skills.map((skill,index)=>{
+            return(
+              <div className="max-h-xs" key={index}>
+                <img src={skill.image} alt="skills"/>
+              </div>
+            )
+          })}
+
+        </motion.div>
+      </div>
+    </section>
+ </InView>
+  );
+};
